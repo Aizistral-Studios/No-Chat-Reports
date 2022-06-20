4215b2022666845d34b7151840e792bba42fa282
Aizistral <VictorShadow-Integral@yandex.ru>
2022-06-20 13:35:47 +0200

    Suppress telemetry

---
+++ src/main/java/com/aizistral/nochatreports/mixins/MixinClientTelemetryManager.java
@@ -0,0 +1,29 @@
+package com.aizistral.nochatreports.mixins;
+
+import java.util.concurrent.Executor;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.mojang.authlib.minecraft.TelemetrySession;
+import com.mojang.authlib.minecraft.UserApiService;
+
+import net.minecraft.client.ClientTelemetryManager;
+
+@Mixin(ClientTelemetryManager.class)
+public class MixinClientTelemetryManager {
+
+	/**
+	 * @reason Privacy.
+	 * @author Aizistral
+	 */
+
+	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/authlib/minecraft/UserApiService;"
+			+ "newTelemetrySession(Ljava/util/concurrent/Executor;)Lcom/mojang/authlib/minecraft/TelemetrySession;"))
+	private TelemetrySession redirectNewTelemetrySession(UserApiService service, Executor executor) {
+		return TelemetrySession.DISABLED;
+	}
+
+}
