a81536ac17fa3717d2d816283b1eb8f0742a3563
Madis <Madis0@users.noreply.github.com>
2022-07-28 22:57:08 +0300

    Remove unused imports

--- src/main/java/com/aizistral/nochatreports/mixins/client/MixinChatListener.java
+++ src/main/java/com/aizistral/nochatreports/mixins/client/MixinChatListener.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
--- src/main/java/com/aizistral/nochatreports/mixins/client/MixinGuiMessageTag.java
+++ src/main/java/com/aizistral/nochatreports/mixins/client/MixinGuiMessageTag.java
@@ -1,6 +1,5 @@
 package com.aizistral.nochatreports.mixins.client;
 
-import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
