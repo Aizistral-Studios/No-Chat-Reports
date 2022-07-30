428632f1b84dce83262834c4d9f5c91c54601e27
MODKILLER1001 <modkiller1001@gmail.com>
2022-07-30 15:32:53 -0400

    2nd attempt at this

--- src/main/java/com/aizistral/nochatreports/ModMenuIntegration.java
+++ src/main/java/com/aizistral/nochatreports/ModMenuIntegration.java
@@ -2,10 +2,12 @@ package com.aizistral.nochatreports;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ConfigScreen::getScreen;
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
     }
 }
