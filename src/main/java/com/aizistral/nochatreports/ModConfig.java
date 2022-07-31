5b3e16910238bb423fef73bc64f335be8eb6e84a
MODKILLER1001 <modkiller1001@gmail.com>
2022-07-31 13:58:53 -0400

    other

--- src/main/java/com/aizistral/nochatreports/ModConfig.java
+++ src/main/java/com/aizistral/nochatreports/ModConfig.java
@@ -1,12 +1,14 @@
 package com.aizistral.nochatreports;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 @Config(name = "NoChatReports")
 public class ModConfig implements ConfigData {
-
     @ConfigEntry.Gui.Tooltip()
     public boolean demandOnServer = true;
     @ConfigEntry.Gui.Tooltip()
@@ -21,8 +23,6 @@ public class ModConfig implements ConfigData {
     public boolean hideGrayChatIndicators = true;
     @ConfigEntry.Gui.Tooltip()
     public boolean hideWarningToast = true;
-    @ConfigEntry.Gui.Tooltip()
-    public boolean suppressVanillaSecurityNotices = true;
     @ConfigEntry.Gui.Tooltip()
     public boolean alwaysHideReportButton = false;
     @ConfigEntry.Gui.Tooltip()
