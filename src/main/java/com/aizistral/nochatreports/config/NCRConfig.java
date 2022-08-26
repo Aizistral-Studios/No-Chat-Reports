304af8d893d9d91bca0e08fec07b812623d65d2e
Aizistral <VictorShadow-Integral@yandex.ru>
2022-08-26 20:08:16 +0200

    Add README file to NCR config folder

--- src/main/java/com/aizistral/nochatreports/config/NCRConfig.java
+++ src/main/java/com/aizistral/nochatreports/config/NCRConfig.java
@@ -1,6 +1,12 @@
 package com.aizistral.nochatreports.config;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
+
+import com.aizistral.nochatreports.NoChatReports;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -33,12 +39,12 @@ public final class NCRConfig {
 
 	public static void load() {
 		common = JSONConfig.loadConfig(NCRCommonConfig.class, NCRCommonConfig::new, NCRCommonConfig.FILE_NAME);
-		common.saveFile();
 
 		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
 			client = JSONConfig.loadConfig(NCRClientConfig.class, NCRClientConfig::new, NCRClientConfig.FILE_NAME);
-			client.saveFile();
 		}
+
+		save();
 	}
 
 	public static void save() {
@@ -46,6 +52,18 @@ public final class NCRConfig {
 
 		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
 			Objects.requireNonNull(client, "Cannot save config because it was not loaded!").saveFile();
+		}
+
+		Path readme = JSONConfig.CONFIG_DIR.resolve("NoChatReports/README.md");
+		try {
+			Files.writeString(readme, """
+					# No Chat Reports
+					You can find updated documentation of configuration files on the wiki:
+					https://github.com/Aizistral-Studios/No-Chat-Reports/wiki/Configuration-Files
+					""", StandardCharsets.UTF_8);
+		} catch (Exception ex) {
+			NoChatReports.LOGGER.fatal("Could not write README file: {}", readme);
+			throw new RuntimeException(ex);
 		}
 	}
 
