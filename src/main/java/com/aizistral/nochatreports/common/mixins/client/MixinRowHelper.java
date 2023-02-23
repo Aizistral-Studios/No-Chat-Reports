c306300e68fd67d365c652d42ffa55aaf0a695e7
Aizistral <admin@aizistral.com>
2023-02-23 15:23:17 +0100

    Fix MixinRowHelper

--- src/main/java/com/aizistral/nochatreports/common/mixins/client/MixinRowHelper.java
+++ src/main/java/com/aizistral/nochatreports/common/mixins/client/MixinRowHelper.java
@@ -8,19 +8,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.aizistral.nochatreports.common.config.NCRConfig;
 
 import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.layouts.LayoutElement;
 import net.minecraft.client.gui.screens.OptionsScreen;
 
 @Mixin(GridLayout.RowHelper.class)
 public abstract class MixinRowHelper {
 
-	@Inject(method = "addChild(Lnet/minecraft/client/gui/components/AbstractWidget;)"
+	@Inject(method = "addChild(Lnet/minecraft/client/gui/layouts/LayoutElement;)"
 			+ "Lnet/minecraft/client/gui/components/AbstractWidget;", at = @At("HEAD"), cancellable = true)
-	private void onAddChild(AbstractWidget widget, CallbackInfoReturnable<AbstractWidget> info) {
+	private void onAddChild(LayoutElement element, CallbackInfoReturnable<AbstractWidget> info) {
 		if (NCRConfig.getClient().disableTelemetry() && NCRConfig.getClient().removeTelemetryButton())
-			if (OptionsScreen.TELEMETRY.equals(widget.getMessage())) {
-				info.setReturnValue(widget);
-			}
+			if (element instanceof Button button)
+				if (OptionsScreen.TELEMETRY.equals(button.getMessage())) {
+					info.setReturnValue(button);
+				}
 	}
 
 }
