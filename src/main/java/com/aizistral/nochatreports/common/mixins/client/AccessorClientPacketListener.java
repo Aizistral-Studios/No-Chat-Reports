1b0c7898e589f2bf123d942ee88b8afea9e05e04
Sarun Intaralawan <sarunint@sarunint.com>
2025-06-23 01:03:19 +0700

    fix: replace `parseCommand` invoker with calls to `CommandDispatcher`

--- src/main/java/com/aizistral/nochatreports/common/mixins/client/AccessorClientPacketListener.java
+++ src/main/java/com/aizistral/nochatreports/common/mixins/client/AccessorClientPacketListener.java
@@ -1,17 +1,21 @@
 package com.aizistral.nochatreports.common.mixins.client;
 
-import com.mojang.brigadier.ParseResults;
+import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.client.multiplayer.ClientSuggestionProvider;
 import net.minecraft.world.entity.player.ProfileKeyPair;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ClientPacketListener.class)
 public interface AccessorClientPacketListener {
 
-	@Invoker("parseCommand")
-	public ParseResults<SharedSuggestionProvider> invokeParseCommand(String string);
+	@Accessor("commands")
+	public CommandDispatcher<ClientSuggestionProvider> getCommands();
+
+	@Accessor("suggestionsProvider")
+	public ClientSuggestionProvider getClientSuggestionProvider();
 
 	@Invoker("setKeyPair")
 	public void invokeSetKeyPair(ProfileKeyPair profileKeyPair);
