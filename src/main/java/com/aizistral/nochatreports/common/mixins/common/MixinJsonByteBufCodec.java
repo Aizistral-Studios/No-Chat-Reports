ae76376f7c704230f877c118c7a4763123136d4b
Aizistral <admin@aizistral.com>
2025-06-26 15:21:30 +0200

    Clean up debug logs in `MixinJsonByteBufCodec`

--- src/main/java/com/aizistral/nochatreports/common/mixins/common/MixinJsonByteBufCodec.java
+++ src/main/java/com/aizistral/nochatreports/common/mixins/common/MixinJsonByteBufCodec.java
@@ -8,8 +8,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.aizistral.nochatreports.common.NCRCore;
 import com.aizistral.nochatreports.common.config.NCRConfig;
 import com.aizistral.nochatreports.common.core.ServerStatusCache;
+import com.aizistral.nochatreports.fabric.NoChatReports;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -30,16 +32,14 @@ public class MixinJsonByteBufCodec {
 
 	@Inject(method = "encode", at = @At("HEAD"), cancellable = true)
 	private void onEncode(ByteBuf buf, JsonElement element, CallbackInfo info) {
-		System.out.println("ONENECODE WORKIN");
-
-		System.out.println("ELEMENT: " + element.toString());
-
 		if (!NCRConfig.getCommon().addQueryData() || !this.isServerStatusElement(element))
 			return;
 
-		System.out.println("ADDING INFO TO BUF");
-
 		info.cancel();
+
+		if (NCRConfig.getCommon().enableDebugLog()) {
+			NCRCore.LOGGER.info("Adding chat report prevention status to ServerStatus packet.");
+		}
 
 		JsonObject object = element.getAsJsonObject();
 		object.addProperty("preventsChatReports", true);
@@ -50,18 +50,17 @@ public class MixinJsonByteBufCodec {
 
 	@Inject(method = "decode", at = @At("RETURN"), cancellable = true)
 	private void onDecode(ByteBuf buf, CallbackInfoReturnable<JsonElement> info) {
-		System.out.println("ONDECODE WORKIN");
 		JsonElement element = info.getReturnValue();
 
 		if (!this.isServerStatusElement(element))
 			return;
 
-		System.out.println("getting INFO frim BUF");
-
 		JsonObject object = element.getAsJsonObject();
 		boolean preventsReports = object.has("preventsChatReports") && object.get("preventsChatReports").getAsBoolean();
 
-		System.out.println("PREVENTS REPORTS: " + (object.has("preventsChatReports") && object.get("preventsChatReports").getAsBoolean()));
+		if (NCRConfig.getCommon().enableDebugLog()) {
+			NCRCore.LOGGER.info("Received chat report prevention status from ServerStatus packet: " + preventsReports);
+		}
 
 		ServerStatusCache.setPreventsReports(preventsReports);
 	}
