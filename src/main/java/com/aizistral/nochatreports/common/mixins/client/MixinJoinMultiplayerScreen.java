b80a6c5fadda4c97efe4ed767a0f3c03639277ef
Ink <inkepicnes3@gmail.com>
2025-10-11 16:32:48 -0400

    Sync with 1.21.9/1.21.10 update

--- src/main/java/com/aizistral/nochatreports/common/mixins/client/MixinJoinMultiplayerScreen.java
+++ src/main/java/com/aizistral/nochatreports/common/mixins/client/MixinJoinMultiplayerScreen.java
@@ -1,25 +1,22 @@
 package com.aizistral.nochatreports.common.mixins.client;
 
+import com.aizistral.nochatreports.common.gui.*;
 import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
 import net.minecraft.client.gui.layouts.LinearLayout;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.aizistral.nochatreports.common.config.NCRConfig;
 import com.aizistral.nochatreports.common.core.ServerSafetyState;
-import com.aizistral.nochatreports.common.gui.AdvancedImageButton;
-import com.aizistral.nochatreports.common.gui.AdvancedTooltip;
-import com.aizistral.nochatreports.common.gui.GUIShenanigans;
-import com.aizistral.nochatreports.common.gui.SwitchableSprites;
 
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import net.minecraft.locale.Language;
-import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -30,58 +27,62 @@ import net.minecraft.resources.ResourceLocation;
 
 @Mixin(JoinMultiplayerScreen.class)
 public abstract class MixinJoinMultiplayerScreen extends Screen {
-	@Shadow @Final
-	private HeaderAndFooterLayout layout;
-	private static final ResourceLocation RELOAD_TEXTURE = ResourceLocation.fromNamespaceAndPath("nochatreports", "textures/gui/config_reload_button.png"),
-			TOGGLE_TEXTURE = ResourceLocation.fromNamespaceAndPath("nochatreports", "textures/gui/ncr_toggle_button.png");
-	private static final Component RELOAD_TOOLTIP = Component.translatable("gui.nochatreports.reload_config_tooltip");
+    @Shadow @Final
+    private HeaderAndFooterLayout layout;
 
-	protected MixinJoinMultiplayerScreen() {
-		super(null);
-		throw new IllegalStateException("Can't touch this");
-	}
+    @Unique private static final ResourceLocation RELOAD_ICON = ResourceLocation
+            .fromNamespaceAndPath("nochatreports", "config_reload");
+    @Unique private static final ResourceLocation RELOAD_ICON_DISABLED = ResourceLocation
+            .fromNamespaceAndPath("nochatreports", "config_reload_disabled");
+    @Unique private static final ResourceLocation NCR_ACTIVE_ICON = ResourceLocation
+            .fromNamespaceAndPath("nochatreports", "ncr_active");
+    @Unique private static final ResourceLocation NCR_ACTIVE_ICON_DISABLED = ResourceLocation
+            .fromNamespaceAndPath("nochatreports", "ncr_active_disabled");
+    @Unique private static final ResourceLocation NCR_INACTIVE_ICON = ResourceLocation
+            .fromNamespaceAndPath("nochatreports", "ncr_inactive");
 
-	@Inject(method = "init", at = @At("HEAD"))
-	private void onInit(CallbackInfo info) {
-		LinearLayout ncrButtons = this.layout.addToFooter(LinearLayout.vertical().spacing(4), layoutSettings -> {
-			layoutSettings.paddingLeft(336);
-		});
+    @Unique private static final Component RELOAD_TOOLTIP = Component.translatable("gui.nochatreports.reload_config_tooltip");
 
-		if (NCRConfig.getClient().showReloadButton()) {
-			var button = new AdvancedImageButton(0, 0, 20, 20,
-					SwitchableSprites.of(GUIShenanigans.getSprites("config_reload_button")),
-					btn -> NCRConfig.load(), CommonComponents.EMPTY, this);
-			button.setTooltip(new AdvancedTooltip(RELOAD_TOOLTIP).setMaxWidth(250));
-			button.active = true;
-			button.visible = true;
-			ncrButtons.addChild(button);
-		}
+    protected MixinJoinMultiplayerScreen() {
+        super(null);
+        throw new IllegalStateException("Can't touch this");
+    }
 
-		if (NCRConfig.getClient().showNCRButton()) {
-			var button = new AdvancedImageButton(0, 0, 20, 20,
-					SwitchableSprites.of(
-							GUIShenanigans.getSprites("ncr_active_button"),
-							GUIShenanigans.getSprites("ncr_inactive_button")
-							).setIndex(NCRConfig.getClient().enableMod() ? 0 : 1),
-					btn -> {
-						NCRConfig.getClient().toggleMod();
-						boolean enabled = NCRConfig.getClient().enableMod();
+    @Inject(method = "init", at = @At("HEAD"))
+    private void onInit(CallbackInfo info) {
+        LinearLayout ncrButtons = this.layout.addToFooter(LinearLayout.vertical().spacing(4), layoutSettings -> {
+            layoutSettings.paddingLeft(336);
+        });
 
-						if (enabled) {
-							((AdvancedImageButton)btn).useSprites(0);
-						} else {
-							((AdvancedImageButton)btn).useSprites(1);
-						}
+        AdvancedIconButton ncrReloadButton = new AdvancedIconButton(0, 0, 20, 20,
+                new SwitchableButtonIcon(new ButtonIconData(
+                        new IconData(RELOAD_ICON, 12, 13),
+                        new IconData(RELOAD_ICON_DISABLED, 12, 13))),
+                btn -> NCRConfig.load(),
+                this);
+        ncrReloadButton.setTooltip(new AdvancedTooltip(RELOAD_TOOLTIP).setMaxWidth(250));
+        ncrReloadButton.active = true;
+        ncrReloadButton.visible = NCRConfig.getClient().showReloadButton();
+        ncrButtons.addChild(ncrReloadButton);
 
-						ServerSafetyState.reset();
-					}, Component.translatable("gui.nochatreports.ncr_toggle"), this);
-			button.setTooltip(new AdvancedTooltip(() -> Component.translatable("gui.nochatreports.ncr_toggle_tooltip",
-					Language.getInstance().getOrDefault("gui.nochatreports.ncr_state_" + (NCRConfig.getClient()
-							.enableMod() ? "on" : "off")))).setMaxWidth(250));
-			button.active = true;
-			button.visible = true;
-			ncrButtons.addChild(button);
-		}
-	}
-
+        AdvancedIconButton ncrToggleButton = new AdvancedIconButton(0, 0, 20, 20,
+                new SwitchableButtonIcon(
+                        new ButtonIconData(
+                                new IconData(NCR_ACTIVE_ICON, 14, 14),
+                                new IconData(NCR_ACTIVE_ICON_DISABLED, 14, 14)),
+                        new ButtonIconData(new IconData(NCR_INACTIVE_ICON, 14, 14))
+                ).setIndex(NCRConfig.getClient().enableMod() ? 0 : 1),
+                btn -> {
+                    NCRConfig.getClient().toggleMod();
+                    ((AdvancedIconButton) btn).setIconIndex(NCRConfig.getClient().enableMod() ? 0 : 1);
+                    ServerSafetyState.reset();
+                },
+                this);
+        ncrToggleButton.setTooltip(new AdvancedTooltip(() -> Component.translatable("gui.nochatreports.ncr_toggle_tooltip",
+                Language.getInstance().getOrDefault("gui.nochatreports.ncr_state_" + (NCRConfig.getClient()
+                        .enableMod() ? "on" : "off")))).setMaxWidth(250));
+        ncrToggleButton.active = true;
+        ncrToggleButton.visible = NCRConfig.getClient().showNCRButton();
+        ncrButtons.addChild(ncrToggleButton);
+    }
 }
