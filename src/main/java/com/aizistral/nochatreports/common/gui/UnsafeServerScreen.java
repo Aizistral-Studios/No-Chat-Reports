25ef48cab2c26768c76b9802c7e2a66421b6a181
tibequadorian <9560587+tibequadorian@users.noreply.github.com>
2025-11-25 19:56:01 +0100

    Adapt warning screens to new GUI

--- src/main/java/com/aizistral/nochatreports/common/gui/UnsafeServerScreen.java
+++ src/main/java/com/aizistral/nochatreports/common/gui/UnsafeServerScreen.java
@@ -7,38 +7,25 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.screens.ChatScreen;
+import net.minecraft.client.gui.layouts.Layout;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.multiplayer.WarningScreen;
 import net.minecraft.network.chat.Component;
 
 @Environment(EnvType.CLIENT)
-public final class UnsafeServerScreen extends AdaptiveWarningScreen {
+public final class UnsafeServerScreen extends WarningScreen {
 	private static final Component TITLE = Component.translatable("gui.nochatreports.signing_required.header").withStyle(ChatFormatting.BOLD);
 	private static final Component CONTENT = Component.translatable("gui.nochatreports.signing_required.contents");
 	private static final Component CHECK = Component.translatable("gui.nochatreports.signing_required.check");
 	private static final Component NARRATION = TITLE.copy().append("\n").append(CONTENT);
+	private final Screen previous;
+
 	private static boolean hideThisSession = false;
 
 	public UnsafeServerScreen(Screen previous) {
-		super(TITLE, CONTENT, CHECK, previous);
-	}
-
-	@Override
-	protected void initButtons(int i) {
-		this.addRenderableWidget(Button.builder(Component.translatable("gui.nochatreports.signing_required.allow_signing"),
-				button -> {
-					ServerSafetyState.setAllowChatSigning(true);
-					this.minecraft.setScreen(this.previous);
-				}).bounds(this.width / 2 - 155, i, 150, 20)
-				.build());
-
-		this.addRenderableWidget(Button.builder(Component.translatable("gui.nochatreports.signing_required.cancel"),
-				button -> {
-					if (this.stopShowing.selected()) {
-						hideThisSession = true;
-					}
-					this.minecraft.setScreen(this.previous);
-				}).bounds(this.width / 2 - 155 + 160, i, 150, 20).build());
+		super(TITLE, CONTENT, CHECK, NARRATION);
+		this.previous = previous;
 	}
 
 	public static void setHideThisSession(boolean hide) {
@@ -49,4 +36,19 @@ public final class UnsafeServerScreen extends AdaptiveWarningScreen {
 		return hideThisSession;
 	}
 
+	@Override
+	protected Layout addFooterButtons() {
+		LinearLayout linearLayout = LinearLayout.horizontal().spacing(8);
+		linearLayout.addChild(Button.builder(Component.translatable("gui.nochatreports.signing_required.allow_signing"), button -> {
+			ServerSafetyState.setAllowChatSigning(true);
+			this.minecraft.setScreen(this.previous);
+		}).build());
+		linearLayout.addChild(Button.builder(Component.translatable("gui.nochatreports.signing_required.cancel"), button -> {
+			if (this.stopShowing.selected()) {
+				hideThisSession = true;
+			}
+			this.minecraft.setScreen(this.previous);
+		}).build());
+		return linearLayout;
+	}
 }
